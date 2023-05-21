950911bb546132d914d0144b8a2ab7d61eaf4c8c
Fintechdevs <113683834+Fintechdevs@users.noreply.github.com>
2023-05-21 19:13:32 +0400

    Update installVPN.sh

--- installVPN.sh
+++ installVPN.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 sudo wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash
